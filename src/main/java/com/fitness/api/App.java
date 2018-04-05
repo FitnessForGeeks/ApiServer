be9ad09716a38b4ba29151bd148fd63c4f4aa14a
TimUntersberger <timuntersberger2@gmail.com>
2018-04-05 08:57:26 +0200

    working apiserver

--- src/main/java/com/fitness/api/App.java
+++ src/main/java/com/fitness/api/App.java
@@ -1,5 +1,9 @@
 package com.fitness.api;
 
+import spark.Spark;
+
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 /**
@@ -10,8 +14,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        get("/Hello", (req, res) -> {
-            return "Yes";
+        // headers to allow cross domain api calls
+        Spark.after((request, response) -> {
+            response.header("Access-Control-Allow-Methods", "*");
+            response.header("Access-Control-Allow-Origin", "*");
         });
+        get("/user", (req, res) -> {
+            HashMap<String, String> responseData = new HashMap();
+            responseData.put("message", "Hello World");
+            return responseData;
+        }, Utility::toJson);
+        post("/user", (req, res) -> {
+            return "hello";
+        }, Utility::toJson);
     }
 }
