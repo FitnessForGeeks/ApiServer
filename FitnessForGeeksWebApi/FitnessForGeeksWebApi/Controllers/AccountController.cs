7c90470138e051ed13d4cfbe2d14292be920af99
TimUntersberger <timuntersberger2@gmail.com>
2018-05-24 13:14:27 +0200

    changed to c#

---
+++ FitnessForGeeksWebApi/FitnessForGeeksWebApi/Controllers/AccountController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using FitnessForGeeksWebApi.Database;
+using System.Collections.Generic;
+using FitnessForGeeksWebApi.Controllers.RequestDataClasses;
+
+namespace FitnessForGeeksWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly AccountManager manager;
+
+        public AccountController()
+        {
+            this.manager = new AccountManager();
+        }
+
+        [HttpGet]
+        public List<Account> Get([FromQuery]int id)
+        {
+            return manager.Get();
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Post([FromBody] LoginPostData data)
+        {
+            if (data.Username == null || data.Password == null)
+            {
+                return StatusCode(400);
+            }
+            Account acc = manager.GetByUsername(data.Username);
+            if (acc != null && acc.Password == data.Password)
+            {
+                Response.Cookies.Append("authKey", acc.AuthKey, new Microsoft.AspNetCore.Http.CookieOptions
+                {
+                    HttpOnly = true
+                });
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("logout")]
+        public IActionResult Post()
+        {
+            Response.Cookies.Delete("authKey");
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateAccountPostData data)
+        {
+            if (manager.Create(data))
+            {
+                return Ok();
+            }
+            return StatusCode(409);
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody]int id)
+        {
+        }
+    }
+}
---
+++ FitnessForGeeksWebApi/FitnessForGeeksWebApi/Database/AccountManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using FitnessForGeeksWebApi.Controllers;
+using FitnessForGeeksWebApi.Utility;
+
+namespace FitnessForGeeksWebApi.Database
+{
+    public class AccountManager : IDatabaseManager<Account>
+    {
+        public List<Account> Get()
+        {
+            var accounts = new List<Account>();
+            MySqlDatabase.ExecuteReader("select * from accounts", reader => {
+                accounts.Add(NewAccountFromReader(reader));
+            });
+            return accounts;
+        }
+
+        private Account NewAccountFromReader(MySqlDataReader reader)
+        {
+            return new Account(
+                MySqlDatabase.GetValueOrNull<int>(reader, "id"),
+                MySqlDatabase.GetValue<string>(reader, "username"),
+                MySqlDatabase.GetValue<string>(reader, "password"),
+                MySqlDatabase.GetValueOrNull<DateTime>(reader, "birthdate"),
+                MySqlDatabase.GetValueOrNull<double>(reader, "weight"),
+                MySqlDatabase.GetValueOrNull<double>(reader, "height"),
+                MySqlDatabase.GetValueOrNull<bool>(reader, "isVerified"),
+                MySqlDatabase.GetValue<string>(reader, "authKey"),
+                MySqlDatabase.GetValue<string>(reader, "firstName"),
+                MySqlDatabase.GetValue<string>(reader, "lastName"),
+                MySqlDatabase.GetValue<string>(reader, "email"),
+                MySqlDatabase.GetValue<string>(reader, "description")
+            );
+        }
+
+        public Account GetByUsername(String username)
+        {
+            Account acc = null;
+            try
+            {
+                MySqlDatabase.ExecuteReader(
+                    $"select * from accounts where username='{username}'",
+                    reader => {
+                        acc = NewAccountFromReader(reader);
+                    },
+                    true
+                );
+            } catch (ResultIsEmptyException ex) {}
+            return acc;
+        }
+
+        public bool Create(CreateAccountPostData data)
+        {
+            bool success = false;
+            MySqlDatabase.ExecuteNoneQuery(
+                $"insert into accounts(username, password, email, authKey) values ('{data.Username}','{data.Password}','{data.Email}','{Hash.Sha256(data.Username + data.Password)}')",
+                count => 
+                {
+                    if (count == 1)
+                        success = true;
+                }
+                );
+            return success;
+        }
+    }
+}
